2b691f52ee1a2098fcca83b82cac5bed51eba37c
Matheus <matheussiqueiraalmeida7@gmail.com>
2025-03-26 10:56:44 -0400

    atualização aula front

---
+++ formulario/main.js
@@ -0,0 +1,4 @@
+const notaform=document.getElementById('notaform');
+const notaInput=document.getElementById('notaInput');
+const dataInput=document.getElementById('dataInput');
+const listaNotas=document.getElementById('listaNotas');
